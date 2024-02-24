814e875b04679c777f67769f74ef67992a9552da
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-25 00:43:29 +0530

    Update db_conn

--- db_conn.php
+++ db_conn.php
@@ -3,10 +3,10 @@
 <?php
 
 $sname = "targetlifter-server.database.windows.net";
-$uname = "targetlifter-server-admin";
+$uname = "targetlifter-server";
 $password = "xN]3PxKgeT9,^NuxN]3PxKgeT9,^Nu";
 
-$db_name = "target-lifter";
+$db_name = "targetlifter000";
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
